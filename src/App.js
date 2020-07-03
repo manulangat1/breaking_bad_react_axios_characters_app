d0e72482793315f06021a96bcbe6b26d136230aa
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-03 21:45:33 +0300

    ch:adding_character_grid:frontend

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React ,{useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './components/styles/main.scss'
 import Header from './components/ui/Header'
 import Footer from './components/ui/Footer'
 
+import CharacterGrid from './components/core/CharacterGrid'
+ import axios from 'axios'
+
 const  App = () => {
+  const [items,setItems] = useState([])
+  const [isLoading,setIsLoading] = useState(true)
+
+
+
+  useEffect(() => {
+    const fetchItems  = async () => {
+      const result = await axios.get(`https://www.breakingbadapi.com/api/characters`)
+      // console.log(result.data)
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+    console.log(items)
+    console.log(isLoading)
+  },[])
   return (
     <main>
     <Header />
     <div className="container maps">
-      <p>ht</p>
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
     <Footer />
     </main>
