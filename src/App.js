865d1e663a7e28dc66eccc03e85d5e1f68dd69d4
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-03 23:02:55 +0300

    ch:adding_search_functionality:backend

--- src/App.js
+++ src/App.js
@@ -5,29 +5,32 @@ import './components/styles/main.scss'
 import Header from './components/ui/Header'
 import Footer from './components/ui/Footer'
 
+import Search from './components/core/Search'
 import CharacterGrid from './components/core/CharacterGrid'
  import axios from 'axios'
 
 const  App = () => {
   const [items,setItems] = useState([])
   const [isLoading,setIsLoading] = useState(true)
+  const [query,setQuery] = useState('')
 
 
 
   useEffect(() => {
     const fetchItems  = async () => {
-    const result = await axios.get(`https://www.breakingbadapi.com/api/characters`)
+    const result = await axios.get(`https://www.breakingbadapi.com/api/characters?name=${query}`)
       // console.log(result.data)
       console.log(result.data)
     setItems(result.data)
     setIsLoading(false)
     }
     fetchItems()
-  },[])
+  },[query])
   return (
     <main>
     <Header />
     <div className="container maps">
+      <Search getQuery={(q) => setQuery(q)}/>
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
     <Footer />
